543aa849e350d480dc176d8d5c0dcc71f1a39611
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-07-08 19:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,10 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 
-var uppercase = "HELLO";
-var lowercase = "hello";
-
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return ('I can\'t hear you!');
-} else if (string.toUpperCase() === uppercase) {
+} else if (string.toUpperCase() === string) {
     return ('YES INDEED!');
 } else { 
   return "I love you, too.";
